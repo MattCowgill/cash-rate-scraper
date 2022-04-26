94df399295be767e6f3825cf25cf855dbfe10b38
“Matt <mattcowgill@gmail.com>
2022-04-26 11:50:03 +1000

    knit readme with GHA

--- R/viz_cash_rate.R
+++ R/viz_cash_rate.R
@@ -6,5 +6,5 @@ suppressPackageStartupMessages({
 cash_rate <- readRDS(file.path("combined_data", "all_data.Rds"))
 
 viz <- cash_rate |>
-  ggplot(aes(x = date, y = cash_rate, col = scrape_date)) +
+  ggplot(aes(x = date, y = cash_rate, col = scrape_date, group = scrape_date)) +
   geom_line()
