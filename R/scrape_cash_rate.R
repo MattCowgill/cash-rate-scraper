c5ccc7302f85a0bce836af66de71a638372be156
“Matt <mattcowgill@gmail.com>
2022-12-04 21:08:05 +1100

    fiddle with scraper

--- R/scrape_cash_rate.R
+++ R/scrape_cash_rate.R
@@ -19,9 +19,9 @@ table_image <- full_image |>
   magick::image_crop(geometry = geometry_area(width = 875 * 3.1,
                                               height = 40 * 3.1,
                                               x_off = 170 * 3.1,
-                                              y_off = 650 * 3.1)) |>
+                                              y_off = 630 * 3.1)) |>
   image_quantize(colorspace = "gray") |>
-  image_transparent(color = "white", fuzz = 48)
+  image_transparent(color = "white", fuzz = 60)
 
 # Extract the characters from the image
 strings <- table_image |>
