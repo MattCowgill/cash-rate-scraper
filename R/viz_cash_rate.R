10b18bee1cc9b4c3f192376141f9a32f67cd62fd
“Matt <mattcowgill@gmail.com>
2023-03-07 13:12:45 +1100

    tweak viz1

--- R/viz_cash_rate.R
+++ R/viz_cash_rate.R
@@ -10,7 +10,19 @@ cash_rate <- readRDS(file.path("combined_data", "all_data.Rds"))
 viz_1 <- cash_rate |>
   ggplot(aes(x = date, y = cash_rate, col = scrape_date, group = scrape_date)) +
   geom_line() +
+  geom_line(data = ~filter(., scrape_date == max(scrape_date)),
+            colour = "red",
+            linewidth = 1.5) +
+  geom_text(data = ~filter(.,
+                           scrape_date == max(scrape_date)) |>
+              filter(date == max(date)),
+            colour = "red",
+            hjust = 0,
+            lineheight = 0.8,
+            aes(label = format(scrape_date, "%e %b\n%Y"))) +
   scale_colour_date(date_labels = "%b '%y") +
+  scale_x_date(expand = expansion(c(0, 0.1)),
+               date_labels = "%b\n%Y") +
   theme_minimal() +
   labs(subtitle = "Expected future cash rate",
        colour = "Expected\nas at:",
